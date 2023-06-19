d820a7ed829d727a69652c17e2d962ba22a3a8c4
Alex Timoncini <126895091+AlexTimoncini@users.noreply.github.com>
2023-06-19 14:41:12 +0200

    created component header and main and imported in app

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <script>
-
+  import HeaderApp from './components/HeaderApp.vue';
+  import MainApp from './components/MainApp.vue';
+  export default{
+    name: 'App',
+    components: {
+      HeaderApp,
+      MainApp
+    }
+  }
 </script>
 
 <template>
-
+  <HeaderApp />
+  <MainApp />
 </template>
 
 <style lang="scss">
---
+++ src/components/HeaderApp.vue
@@ -0,0 +1,13 @@
+<script>
+    export default{
+        name: 'HeaderApp'
+    }
+</script>
+
+<template>
+    
+</template>
+
+<style lang="scss" scoped>
+    
+</style>
