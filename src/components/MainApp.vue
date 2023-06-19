831f037a6fb0bb3c589af8faf1119fef755d8991
Alex Timoncini <126895091+AlexTimoncini@users.noreply.github.com>
2023-06-19 15:19:40 +0200

    added main card grid component that get response from api

--- src/components/MainApp.vue
+++ src/components/MainApp.vue
@@ -1,11 +1,17 @@
 <script>
+    import MainCardGrid from './MainCardGrid.vue';
     export default{
-        name: 'MainApp'
+        name: 'MainApp',
+        components: {
+            MainCardGrid,
+        }
     }
 </script>
 
 <template>
-    
+    <main>
+        <MainCardGrid />
+    </main>
 </template>
 
 <style lang="scss" scoped>
---
+++ src/components/MainCardGrid.vue
@@ -0,0 +1,28 @@
+<script>
+    import axios from 'axios';
+    export default{
+        name: 'MainCardGrid',
+        data(){
+            return{
+                cardList: [],
+            }
+        },
+        created(){
+            axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?archetype=Lyrilusc').then( (response) => {
+                this.cardList = response.data.data;
+                console.log(this.cardList)
+            })
+            .catch(function (error){
+                console.log(error);
+            });
+        }
+    }
+</script>
+
+<template>
+    <h1>{{ cardList[1].id }}</h1>
+</template>
+
+<style lang="scss" scoped>
+    
+</style>
