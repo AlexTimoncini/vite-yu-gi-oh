8113a2aabeecf6f560d3741dd453b3585918800f
Alex Timoncini <126895091+AlexTimoncini@users.noreply.github.com>
2023-06-20 13:36:18 +0200

    added hover effect on cardelement, it will flip and show the ddefault back

--- src/components/MainCard.vue
+++ src/components/MainCard.vue
@@ -10,16 +10,41 @@
 
 <template>
     <li>
-        <img :src="img" alt="" loading="lazy">
+        <div class="card">
+            <img :src="img" alt="" loading="lazy" class="front">
+            <img src="../assets/card_back.png" alt="yugioh card back" class="back">
+        </div>
     </li>
 </template>
 
 <style lang="scss" scoped>
     li{
         width: 300px;
+        position: relative;
+        height: 440px;
+        cursor: pointer;
 
-        img{
-            width: 100%;
+        .card{
+            transition: all 0.33s linear;
+            transform-style: preserve-3d;
+
+            img{
+                width: 100%;
+                position: absolute;
+            }
+    
+            .front{
+                z-index: 2;
+                backface-visibility: hidden;
+            }
+    
+            .back{
+                z-index: 1;
+                transform: rotateY(180deg);
+            }
+        }
+        &:hover .card{
+            transform: rotateY(180deg);
         }
 
     }
